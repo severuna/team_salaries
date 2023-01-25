fe49d567244c90416cea2c3b031ce8bfe6ecafab
Severuna <oxxxyfomenkova@gmail.com>
2023-01-25 23:18:11 +0200

    create functions for receiving the data of salary forms, create elements for displaying the received data, add styles to them

--- script/index.js
+++ script/index.js
@@ -1,11 +1,37 @@
+// function to display each form field
+const showDataItem = (data, nameItem) => {
+    // create an element to display form field data
+    const dataItem = document.createElement('p');
+    // set the text content of the element - the name and value of the form field
+    dataItem.textContent = `${nameItem}: ${data.value}`;
+    // return the element
+    return dataItem;
+};
+// getting form field data and passing it for display
+const showInputItem = (data) => {
+    // create an element to display form data
+    const inputItem = document.createElement('div');
+    // add a class for the container
+    inputItem.classList = 'out-item';
+    // add the result of the display function to the element
+    inputItem.append(showDataItem(data.specialization, 'Specialization'), showDataItem(data.salary, 'Salary'), showDataItem(data.tax, 'Tax'));
+    // return the element
+    return inputItem;
+};
 // the salary form was thrown out
 const salariesForm = document.querySelector('#salaries');
-// added an event - obtaining a specialty - salary and tax per click
+// call the button to receive the data of the instance salary positions
 const addSpec = document.querySelector('#addSpec');
+// added an event - obtaining a specialty - salary and tax per click
 addSpec.addEventListener('click', (e) => {
+    // cancellation of the event by default
     e.preventDefault();
-    // while outputting to the console, in the future it will be transferred for output to the corresponding block and will be transferred to the calculation function
-    for(let i = 0; i < salariesForm.length; i++) {
-        console.log(salariesForm[i].value)
-    }
-})
+    // creating a container to display form data
+    const container = document.createElement('div');
+    // add a class for the container
+    container.classList = 'out-container';
+    // add to the container the result of the function of transferring and displaying the data of the form fields
+    container.append(showInputItem(salariesForm))
+    // add the result of functions of receiving, transmitting and displaying form data in the DOM
+    document.querySelector('.entered-out').append(container);
+});
