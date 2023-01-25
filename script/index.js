568ae87831b0f3176649f4e9577a2c93abdd43c8
Severuna <oxxxyfomenkova@gmail.com>
2023-01-25 16:58:45 +0200

    call of the first form and the first features of the function of obtaining data on the specialty, salary and tax

--- script/index.js
+++ script/index.js
@@ -1,2 +1,11 @@
-
-
+// the salary form was thrown out
+const salariesForm = document.querySelector('#salaries');
+// added an event - obtaining a specialty - salary and tax per click
+const addSpec = document.querySelector('#addSpec');
+addSpec.addEventListener('click', (e) => {
+    e.preventDefault();
+    // while outputting to the console, in the future it will be transferred for output to the corresponding block and will be transferred to the calculation function
+    for(let i = 0; i < salariesForm.length; i++) {
+        console.log(salariesForm[i].value)
+    }
+})
